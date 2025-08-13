6e8481b7a678dcde542e51e3f7d30b742ec91281
Ozzy-ZY <zyadhimself1@gmail.com>
2025-08-13 18:24:48 +0300

    add PUT endpoint to product with simple validation & changed the response code from 200 to 204 in the POST endpoint

--- src/main/java/org/product/api/ProductController.java
+++ src/main/java/org/product/api/ProductController.java
@@ -13,9 +13,10 @@ import org.product.data.model.Product;
 import org.jboss.resteasy.reactive.RestResponse.ResponseBuilder;
 
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.Collection;
-
 @Path("/product")
 public class ProductController {
     private final Repository repository;
@@ -37,8 +38,23 @@ public class ProductController {
             return RestResponse.status(400);
         }
         repository.addProduct(product);
+        return RestResponse.created(URI.create("/product/" + product.getId()));
+    }
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public RestResponse updateProduct(@QueryParam("id") int id,Product product){
+        if(repository.getProduct(id) == null){
+            return  RestResponse.status(404);
+        }
+        if (!Product.isValid(product)) {
+            return RestResponse.status(400,"Product is not valid");
+        }
+        product.setId(id);
+        repository.updateProduct(product);
         return ResponseBuilder.ok()
-                .header("Location", "/product/" + product.getId())
+                .header("Location", "/product/" + id)
                 .build();
     }
+
+
 }
