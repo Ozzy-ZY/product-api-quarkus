f028b47aff1448638635435501f5e9d362de2ab0
Ozzy-ZY <zyadhimself1@gmail.com>
2025-08-13 16:52:21 +0300

    add a GET product endpoint

---
+++ src/main/java/org/product/api/ProductController.java
@@ -0,0 +1,33 @@
+package org.product.api;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import io.vertx.core.http.HttpServerRequest;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import org.jboss.resteasy.reactive.RestResponse;
+import org.product.data.Repository;
+import org.product.data.RepositoryImpl;
+import org.product.data.model.Product;
+import org.jboss.resteasy.reactive.RestResponse.ResponseBuilder;
+
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@Path("/product")
+public class ProductController {
+    private final Repository repository;
+
+    public ProductController() {
+        this.repository = RepositoryImpl.getInstance();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<Product> getProducts() {
+        return repository.getProducts();
+    }
+
+}
